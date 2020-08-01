3253886e61d769cfb2e7d58c1449a6c126aedd71
JacksonBurns <33505528+JacksonBurns@users.noreply.github.com>
2020-08-01 15:14:43 -0400

    code examples week 2 - fix indents

--- Code Examples/Week-2/makePretty.m
+++ Code Examples/Week-2/makePretty.m
@@ -3,16 +3,16 @@ function makePretty(intitle,inxlabel,inylabel)
 % all to the currently active figure, along with other aesthetic changes
 % all three entered values must be strings
 	% turn on background lines
-    grid on
+	grid on
 	% turn on borders on top and right
-    box on
+	box on
 	% find the current axes so that we may modify them
-    ax = gca;
+	ax = gca;
 	% make axes thicker, easier to see
-    ax.XAxis.LineWidth = 2;
-    ax.YAxis.LineWidth = 2;
+	ax.XAxis.LineWidth = 2;
+	ax.YAxis.LineWidth = 2;
 	%assign other components
-    title(intitle)
-    xlabel(inxlabel)
-    ylabel(inylabel)
+	title(intitle)
+	xlabel(inxlabel)
+	ylabel(inylabel)
     
